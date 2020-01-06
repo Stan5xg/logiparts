b9f9aa0addaaf1c5432668dea13b45d343fbd8c0
sergei <sudostan@gmail.com>
2020-01-06 22:31:26 +0300

    initial commit

---
+++ src/main/java/com/carbontec/logiparts/LogipartsApplication.java
@@ -0,0 +1,13 @@
+package com.carbontec.logiparts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogipartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogipartsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carbontec/logiparts/LogipartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carbontec.logiparts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogipartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
