15970ae07f64510540336e30da6d7a2434357c8f
sergei <sudostan@gmail.com>
2020-01-18 23:53:47 +0300

    Minor changes

--- src/main/java/com/carbontec/logiparts/controllers/RegistationController.java
+++ src/main/java/com/carbontec/logiparts/controllers/RegistationController.java
@@ -6,13 +6,14 @@ import com.carbontec.logiparts.services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
-import javax.validation.Valid;
 
 @Controller
 public class RegistationController {
